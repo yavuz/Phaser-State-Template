87397821e30835cf6b370f64fd799e07881d6818
yavuz <yvzyldrm@gmail.com>
2014-05-11 05:22:23 +0300

    color format change
    background color format change

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 menuState = {
 	create:function() {
-		this.game.stage.backgroundColor = '#3498db';
+		this.game.stage.backgroundColor = 0x3498db;
 
 		//buttons
 		var buttonTextStyle =  {
@@ -10,7 +10,7 @@ menuState = {
 
 		playButton = this.game.add.button(-this.game.world.centerX,300, "menuButton", this.playBtn);
 		playButtonText = this.game.add.text(playButton.x, playButton.y+15, "MENU", buttonTextStyle);
-		
+
 		// button and text tween
         this.game.add.tween(playButton).to( { x: this.game.world.centerX-173 }, 500, Phaser.Easing.Bounce.Out, true);
         this.game.add.tween(playButtonText).to( { x: this.game.world.centerX-70 }, 500, Phaser.Easing.Bounce.Out, true);
@@ -20,7 +20,7 @@ menuState = {
 	playBtn:function() {
 		game.state.start("play");
 	},
-	
+
 	update:function() {
 
 	}
--- js/play.js
+++ js/play.js
@@ -1,6 +1,6 @@
 playState = {
 	create:function() {
-		this.game.stage.backgroundColor = "#40d47e";
+		this.game.stage.backgroundColor = 0x40d47e;
 
 		//buttons
 		var buttonTextStyle =  {
@@ -10,7 +10,7 @@ playState = {
 
 		playButton = this.game.add.button(this.game.world.width,100, "menuButton", this.playBtn);
 		playButtonText = this.game.add.text(this.game.world.width, playButton.y+15, "GAME", buttonTextStyle);
-		
+
 		// button and text tween
 		this.game.add.tween(playButton).to( { x: this.game.world.centerX-173 }, 400, Phaser.Easing.Bounce.Out, true);
         this.game.add.tween(playButtonText).to( { x: this.game.world.centerX-70 }, 400, Phaser.Easing.Bounce.Out, true);
@@ -20,7 +20,7 @@ playState = {
 	playBtn:function() {
 		game.state.start("menu");
 	},
-	
+
 	update:function() {
 
 	}
